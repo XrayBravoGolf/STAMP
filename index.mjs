968ba33759db0edc985597270f8d2f88da362ea4
XrayBravoGolf <xuxiaoyao213@gmail.com>
2022-07-04 20:42:37 -0700

    always stop watch on any error

--- index.mjs
+++ index.mjs
@@ -3,20 +3,26 @@ import { startWebHookServer } from './server.mjs'
 import { authorizeOAuth, listLabels, startWatching, stopWatching } from './gmail.mjs';
 import { setHistoryId } from './history.mjs'; //! STATEFUL SINGLEGTON
 
-const PORT = 8443;
-// allow GCP to use our server
-startWebHookServer(PORT);
-/*
-await authorizeOAuth();
-setHistoryId(await startWatching());
-// call watch and refresh the history id every 24 hours
-setInterval(async () => {
+try {
+    const PORT = 8443;
+    // allow GCP to use our server
+    startWebHookServer(PORT);
+    /*
+    await authorizeOAuth();
     setHistoryId(await startWatching());
-}, 24 * 60 * 60 * 1000);
-*/
-// stop gmail watch on SIGINT
-process.on('SIGINT', async () => {
-    console.log('SIGINT received, stopping gmail watch');
+    // call watch and refresh the history id every 24 hours
+    setInterval(async () => {
+        setHistoryId(await startWatching());
+    }, 24 * 60 * 60 * 1000);
+    */
+    // stop gmail watch on SIGINT
+    process.on('SIGINT', async () => {
+        console.log('SIGINT received, stopping gmail watch');
+        await stopWatching();
+        process.exit();
+    });
+} catch (err) {
     await stopWatching();
-    process.exit();
-});
+    console.error(err);
+    throw err;
+}
